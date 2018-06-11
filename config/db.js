75986516b000ea5b0c679947a72ef51d171d0106
Seidl, Jaryn <jms844@ftr.com>
2018-06-11 12:02:20 -0600

    minor updates

---
+++ config/db.js
@@ -0,0 +1,16 @@
+var config = {
+    WfmServer: {
+        user:'s_sfuser'
+        ,password: 'spotfire'
+        ,server:'SQLINFWWDVP01\\DCPRD01'
+        ,database:'WfmData'
+    },
+    eWfmServer:{
+        user:'workforceuser'
+        ,password: 'Frontier1'
+        ,server:'SQLINFWWDVP59\\DCPRD59'
+        ,database:'WFM'
+    }
+}
+
+module.exports = config;
